b6c08b41e2a433103d3c44acd3c4233af102d2b5
flypotwedg <lymarc0808@gmail.com>
2022-04-26 18:07:16 -0500

    more code, just in case laptop decides to die

--- app/src/main/java/com/example/ordcancchang/orderAlter.java
+++ app/src/main/java/com/example/ordcancchang/orderAlter.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class orderAlter extends AppCompatActivity {
+
+    Button changeButt;
+    Button cancelButt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_alter);
+
+        changeButt=(Button)findViewById(R.id.change);
+        cancelButt=(Button)findViewById(R.id.cancel);
+
 
         //check date and time to see if before 24 hours of appointment
         //if true, allow the change or cancellation
@@ -21,10 +31,25 @@ public class orderAlter extends AppCompatActivity {
     }
     public void changeAct()
     {
-
+        Intent change=new Intent(this, orderChange.class);
+        startActivityForResult(change,1);
     }
     public void cancelAct()
     {
-
+        Intent cancel=new Intent(this, orderCancel.class);
+        startActivityForResult(cancel,2);
+    }
+    protected void onActivityResult(int reqCode, int resCode, Intent data)
+    {
+        super.onActivityResult(reqCode, resCode, data);
+        switch(reqCode) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                Toast.makeText(this, "An error occurred while saving your changes. Please try again", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
